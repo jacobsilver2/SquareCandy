c0affb2a3dd97ef965e4cbb8846d221c67929160
jacobsilver2 <jacobsilver2@mac.com>
2018-09-18 11:55:13 -0400

    getting started with index.  putting together the store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
+//! ────────────────────────────────────────────────────────────────────────────────
+//! Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+//! ────────────────────────────────────────────────────────────────────────────────
+//! CSS
 import './index.css';
+
+//! ────────────────────────────────────────────────────────────────────────────────
+//! Components
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+//! ────────────────────────────────────────────────────────────────────────────────
+//! Reducers
+
+
+//? Combine Reducers
+
+//? Setup Redux Devtools
+
+//? Create Redux Store With Thunk Middleware
+
+//? Get The App Going
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
