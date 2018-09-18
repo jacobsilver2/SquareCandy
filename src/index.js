e7df211b14c99a0fff21fc652b6ac60cd4de14e1
jacobsilver2 <jacobsilver2@mac.com>
2018-09-18 19:22:00 -0400

    wrote index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-//! ────────────────────────────────────────────────────────────────────────────────
 //! Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -7,26 +6,36 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
-//! ────────────────────────────────────────────────────────────────────────────────
 //! CSS
 import './index.css';
 
-//! ────────────────────────────────────────────────────────────────────────────────
 //! Components
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//! ────────────────────────────────────────────────────────────────────────────────
 //! Reducers
-
+import productsReducer from './store/reducers/products';
 
 //? Combine Reducers
+const rootReducer = combineReducers({ products: productsReducer });
 
 //? Setup Redux Devtools
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 //? Create Redux Store With Thunk Middleware
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 //? Get The App Going
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//? Hook App Up To The DOM
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
