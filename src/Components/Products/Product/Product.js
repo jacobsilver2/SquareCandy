d9a8385179060ad60121cee3f589ab45ae428f75
jacobsilver2 <jacobsilver2@mac.com>
2018-09-20 09:11:23 -0400

    added click/unclick handler to display clicked card in a modal

--- src/Components/Products/Product/Product.js
+++ src/Components/Products/Product/Product.js
@@ -1,23 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Product.css'
-import Button from '../../UI/Button/Button'
-const product = ({name, description, image}) => {
-    let style = { 
-        backgroundImage: 'url(' + image + ')',
-    };
-    return (
-        <div className={classes.card}>
-        <article>
-            <header style={style} id={image} className={classes.cardheader}>
-            </header>
-            <div className={classes.cardbody}>
-                <h2>{name}</h2>
-                <p className={classes.bodycontent}>{description}</p>
-                <Button btnType="Success">Add To Cart</Button>
-            </div>
-        </article>
-        </div>
-    )
-};
+import Button from '../../UI/Button/Button';
+import Aux from '../../../hoc/Aux/Aux'
+import Modal from '../../UI/Modal/Modal';
 
-export default product;
+class Product extends Component {
+    state = { 
+        clicked: false
+     }
+
+    handleClick = () => {
+        this.setState({
+            clicked: true
+        });
+    }
+
+    handleUnclick = () => {
+        this.setState({
+            clicked: false
+        });
+    }
+
+    render() {
+        let {name, description, image} = this.props
+        let style = { 
+            backgroundImage: 'url(' + image + ')',
+        };
+
+        const unClickedCard = (
+            <div className={classes.card} onClick={this.handleClick}>
+            <article>
+                <header style={style} id={image} className={classes.cardheader}>
+                </header>
+                <div className={classes.cardbody}>
+                    <h2>{name}</h2>
+                    <p className={classes.bodycontent}>{description}</p>
+                    <Button btnType="Success">Add To Cart</Button>
+                </div>
+            </article>
+            </div> 
+        )
+
+        const clickedCard = (
+            <div className={classes.card} onClick={this.handleClick}>
+            <article>
+                <header style={style} id={image} className={classes.cardheader}>
+                </header>
+                <div className={classes.cardbody}>
+                    <h2>{name}</h2>
+                    <p className={classes.bodycontent}>{description}</p>
+                    <Button btnType="Success">CARD WAS CLICKED</Button>
+                </div>
+            </article>
+            </div> 
+        )
+
+        
+
+        return (
+            <Aux>
+                <Modal show={this.state.clicked} modalClosed={this.handleUnclick}>
+                    {clickedCard}
+                </Modal>
+                {unClickedCard}
+            </Aux>
+        );
+    }
+}
+
+export default Product;
